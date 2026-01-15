f0fc443c844d7e3d7a099949d5b7a8fe0059d1ef
tanmaykumarchaki <chakitanmay45@gmail.com>
2026-01-15 22:13:33 +0530

    Creating vectorimg function

--- image_loading.py
+++ image_loading.py
@@ -1,5 +1,6 @@
 import numpy as np
 import torch
+import torchvision.transforms as transforms
 import PIL.Image as img
 import dis
 
@@ -32,10 +33,24 @@ def tensorimg(image_variable):
 
 print("Tensoring Image Function",dis.dis(tensorimg))
 
-def vectorimg(image_variable):
-    image = img.open(image_variable)
-    img_vector = torch.flatten(torch.from_numpy(np.array(image)))
-    return img_vector
 
-print("Vectorising Function",dis.dis(vectorimg))
+def vectorimg(image_list):
+    tensor_list = []
 
+    transform = transforms.Compose([
+        transforms.Resize((128, 128)), # Resize the images into 128 x 128
+        transform.ToTensor()
+    ])
+
+    for _img_ in image_list:
+        _img_ = img.open(_img_).convert('RGB')
+        tensor_img = transform(_img_)
+        tensor_list.append(tensor_img)
+
+        for i in tensor_list:
+            vectors =[i.view(-1) for i in tensor_list]
+            vector_tensor = torch.stack(vectors) # Stacking all vectors into a single tensor 
+
+        return vector_tensor
+
+print("Vectorizing Image Function =", dis.dis(vectorimg))
