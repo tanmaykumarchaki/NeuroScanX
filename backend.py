52a4ae7ddc19c807e70b693cae6dbf8d7df394e9
tanmaykumarchaki <chakitanmay45@gmail.com>
2026-01-17 15:45:57 +0530

    backend Developerinfo generated !

--- backend.py
+++ backend.py
@@ -1,9 +1,31 @@
-
+from pydantic import BaseModel
 from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
+class DeveloperInfo(BaseModel):
+    name: str 
+    date_of_birth: str 
+    product_name: str 
+    product_type : str 
+    domain : str 
 
+
+
+
+@app.get("/")
 def greet():
-    return  "Welcome to the Backend Service!"
+    return  "Welcome to the NeuroScan Service!"
+
+
+@app.get("/Developer Info")
+def get_details():
+    return  DeveloperInfo(
+        name = "Tanmay Kumar Chaki",
+        date_of_birth= "29-Dec-2003",
+        product_name="NeuroScan",
+        product_type="Project",
+        domain="Machine Leanring and Data Science"
+
+    )
+
